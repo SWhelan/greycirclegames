71924b5f65a776fb81799e3e94d6aa0e48a378c5
SWhelan <slw96@case.edu>
2015-09-27 17:57:05 -0400

    Initial Commit.

---
+++ src/main/java/Main.java
@@ -0,0 +1,19 @@
+import static spark.Spark.get;
+import static spark.SparkBase.port;
+
+public class Main {
+
+    public static void main(String[] args) {
+        port(getHerokuAssignedPort());
+        get("/hello", (req, res) -> "Hello World");
+    }
+
+    static int getHerokuAssignedPort() {
+        ProcessBuilder processBuilder = new ProcessBuilder();
+        if (processBuilder.environment().get("PORT") != null) {
+            return Integer.parseInt(processBuilder.environment().get("PORT"));
+        }
+        return 4567;
+    }
+
+}
