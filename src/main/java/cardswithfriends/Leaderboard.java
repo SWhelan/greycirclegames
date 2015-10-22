fcff6f8c1bd1b6c72ec23790b294631b4278baf0
George Hodulik <ghodulik95@gmail.com>
2015-10-21 20:15:59 -0400

    Added to Leaderboard class to match the design document

--- src/main/java/cardswithfriends/Leaderboard.java
+++ src/main/java/cardswithfriends/Leaderboard.java
@@ -1,7 +1,9 @@
 package cardswithfriends;
 
 import java.io.Serializable;
+import java.util.Map;
 
 public class Leaderboard implements Serializable{
-
+	public Map<Integer, Integer[]> gameStats;
+	public Map<User, Integer[]> friendStats;
 }
