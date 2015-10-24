a75ca831b04ff140c77437ab406d6140b80fe37c
Thomas <tpech46@gmail.com>
2015-10-24 16:44:27 -0400

    .

--- src/main/java/cardswithfriends/User.java
+++ src/main/java/cardswithfriends/User.java
@@ -6,6 +6,9 @@ public class User implements Player,Serializable {
 	private static final long serialVersionUID = -8255733091222689114L;
 	private int userId;
 	private String userName;
+	private String password;
+	private String salt;
+	private String email;
 	
 	public User(int userId, String userName){
 		this.userId = userId;
