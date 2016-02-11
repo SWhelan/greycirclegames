89becf916b64795872ff07843ae4b4b2ed832012
SWhelan <slw96@case.edu>
2016-02-11 00:18:18 -0500

    Issue #35 Moved all tests out of Test.java
    I think that will be all for this task but probably need to figure out how
    to test the front end.

--- src/test/java/UserTest.java
+++ src/test/java/UserTest.java
@@ -1,17 +1,8 @@
-import java.util.HashSet;
-import java.util.List;
-import java.util.Set;
-
 import greycirclegames.User;
-import greycirclegames.games.card.Card;
-import greycirclegames.games.card.Pile;
 import spark.utils.Assert;
 
-public class Test {
-    public void testing() {
-		Assert.isTrue(true, "The first test!");
-    }
-    
+public class UserTest {
+   
     public void testHashPassword(){
     	String test = User.hashPassword("salt", "password");
     	Assert.isTrue(test.equals(User.hashPassword("salt", "password")), "This method should return the same thing every time the same inputs are used.");
@@ -29,18 +20,5 @@ public class Test {
     	Assert.isTrue(user.passwordMatches("password"), "Method should return true for password matches.");
     	Assert.isTrue(!user.passwordMatches("not the right password"), "Method should return false for incorrect passwords.");
     }  
-    
-    /*
-     * TEST Pile
-     */
-    public void testPileMakeDeck(){
-    	Pile p = Pile.makeDeck("Test Deck");
-    	
-    	Assert.isTrue(p.size()==52, "Deck should be 52 cards");
-    	
-    	List<Card> cards = p.getCards();
-    	Set<Card> cardSet = new HashSet<Card>(cards);
-    	Assert.isTrue(cardSet.size()==52, "Deck should be 52 unique cards");
-    }
     
 }
---
+++ src/test/java/games/card/PileTest.java
@@ -0,0 +1,21 @@
+package games.card;
+
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+import greycirclegames.games.card.Card;
+import greycirclegames.games.card.Pile;
+import spark.utils.Assert;
+
+public class PileTest {
+    public void testPileMakeDeck(){
+    	Pile p = Pile.makeDeck("Test Deck");
+    	
+    	Assert.isTrue(p.size()==52, "Deck should be 52 cards");
+    	
+    	List<Card> cards = p.getCards();
+    	Set<Card> cardSet = new HashSet<Card>(cards);
+    	Assert.isTrue(cardSet.size()==52, "Deck should be 52 unique cards");
+    }
+}
