5602358d5f9d8934208ed3831d009dc688adceca
SWhelan <slw96@case.edu>
2016-02-21 20:23:22 -0500

    Issue #45 Just removed the returning of boolean
    as no one was checking it anyway.

--- src/main/java/greycirclegames/EmailService.java
+++ src/main/java/greycirclegames/EmailService.java
@@ -19,31 +19,31 @@ public class EmailService {
 	private static final String PASSWORD = "Ek7cChbw";
 	private static final String OUT_SMTP = "mail.greycirclegames.com";
 	
-	public static boolean sendNewFriendMail(String to, String friendUsername){
-		return sendMail(new Options(to, friendUsername + " has added you in Grey Circle Games.", friendUsername + " has added you in Grey Circle Games.", TemplateHandler.FRIENDS_ROUTE));
+	public static void sendNewFriendMail(String to, String friendUsername){
+		sendMail(new Options(to, friendUsername + " has added you in Grey Circle Games.", friendUsername + " has added you in Grey Circle Games.", TemplateHandler.FRIENDS_ROUTE));
 	}
 	
-	public static boolean sendNewGameMail(String to, String gameType, String url){
-		return sendMail(new Options(to, "A new " + gameType + " game has been started with you.", "A " + gameType + " game has been started.", url));
+	public static void sendNewGameMail(String to, String gameType, String url){
+		sendMail(new Options(to, "A new " + gameType + " game has been started with you.", "A " + gameType + " game has been started.", url));
 	}
 	
-	public static boolean sendTurnMail(String to, String gameType, String url){
-		return sendMail(new Options(to, "It is your turn.", "Everyone is waiting very patiently for you to make your cunning move.", url));
+	public static void sendTurnMail(String to, String gameType, String url){
+		sendMail(new Options(to, "It is your turn.", "Everyone is waiting very patiently for you to make your cunning move.", url));
 	}
 	
-    public static boolean sendSkippedTurnMail(String to, String gameType, String url){
-        return sendMail(new Options(to, "Your turn has been skipped.", "We tried our darndest, but we couldn't find you any valid moves! Your turn was skipped.", url));
+    public static void sendSkippedTurnMail(String to, String gameType, String url){
+        sendMail(new Options(to, "Your turn has been skipped.", "We tried our darndest, but we couldn't find you any valid moves! Your turn was skipped.", url));
     }
 	
-	public static boolean sendGameOverMail(String to, String url){
-		return sendMail(new Options(to, "A game has ended.", "Rematch?", url));
+	public static void sendGameOverMail(String to, String url){
+		sendMail(new Options(to, "A game has ended.", "Rematch?", url));
 	}
 	
-	public static boolean sendPokeMail(String to, String url){
-		return sendMail(new Options(to, "Reminder - It is your turn!", "Someone else in the game has requested to remind you that it is your turn.", url));
+	public static void sendPokeMail(String to, String url){
+		sendMail(new Options(to, "Reminder - It is your turn!", "Someone else in the game has requested to remind you that it is your turn.", url));
 	}
 	
-	public static boolean sendMail(Options mail){
+	public static void sendMail(Options mail){
 		Properties properties = System.getProperties();
 		properties.put("mail.transport.protocol", "smtp");
 		properties.put("mail.smtp.host", OUT_SMTP);
@@ -67,10 +67,8 @@ public class EmailService {
 				transport.sendMessage(message, message.getAllRecipients());
 				transport.close();
 			}
-			return true;
 		} catch (MessagingException e) {
 			e.printStackTrace();
-			return false;
 		}
 	}
 	
